1b52474fd1f9821c0f8169d39af8fcb6da668a8a
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-03-15 10:59:15 +0100

    Make monitors automatically bright after restart

--- autostart_applications.lua
+++ autostart_applications.lua
@@ -7,3 +7,19 @@ awful.spawn.with_shell("killall redshift ; sleep 2 ; redshift") -- has a delay a
 for i, command in ipairs(settings.run_on_startup) do
     awful.spawn.with_shell(command)
 end
+
+
+-- set screen brightness to be bright in case it was set to dark from last session
+if settings.darken_screens_with_DDC_CI then
+    local file = io.open(config_dir .. "last_screen_brightness_state.txt", "r")
+    local content = file:read("*a")
+    file:close()
+
+    if content == "dark" then
+        awful.spawn.with_shell("ddcutil --display 1 setvcp 10 100 && ddcutil --display 2 setvcp 10 100")
+        -- save brightness state to file
+        local file = io.open(config_dir .. "last_screen_brightness_state.txt", "w")
+        file:write("bright")
+        file:close()
+    end
+end
