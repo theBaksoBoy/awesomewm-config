a93bcc9e45d1a63065145ad41f75c5a19f630db0
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-05-27 16:04:01 +0200

    reversed sensible ordering due to everything exploding

--- get_wallpaper_to_use.lua
+++ get_wallpaper_to_use.lua
@@ -1,12 +1,12 @@
 
 local wallpaperPathsForDaysOfWeek = {
+    [0] = "Mystic_Lands_characters",  -- SUNDAY
     [1] = "Mystic_Lands_characters",      -- Monday
     [2] = "cube_pondering",      -- Tuesday
     [3] = "spiral-space Cotton",      -- Wednesday
     [4] = "spiral-space Cotton", -- Thursday
     [5] = "CottonExplore", -- Friday
     [6] = "Mystic_Lands_background"   -- Saturday
-    [0] = "Mystic_Lands_characters",  -- Sunday
 }
 
 local dayOfWeek = tonumber(os.date("%w")) -- get weekday as an int. Starts on sunday as it is cringe
