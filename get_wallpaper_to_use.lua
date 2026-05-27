bfbf9d4ec5ebd8dfbb17cdd9d5bdcb2062b9e0ea
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-05-27 15:56:55 +0200

    added and removed and changed backgrounds

--- get_wallpaper_to_use.lua
+++ get_wallpaper_to_use.lua
@@ -1,12 +1,12 @@
 
 local wallpaperPathsForDaysOfWeek = {
-    [0] = "Cotton_Nixie_space",  -- SUNDAY
-    [1] = "cube_pondering",      -- Monday
+    [0] = "Mystic_Lands_characters",  -- SUNDAY
+    [1] = "Mystic_Lands_characters",      -- Monday
     [2] = "cube_pondering",      -- Tuesday
-    [3] = "cube_pondering",      -- Wednesday
+    [3] = "spiral-space Cotton",      -- Wednesday
     [4] = "spiral-space Cotton", -- Thursday
-    [5] = "spiral-space Cotton", -- Friday
-    [6] = "Cotton_Nixie_space"   -- Saturday
+    [5] = "CottonExplore", -- Friday
+    [6] = "Mystic_Lands_background"   -- Saturday
 }
 
 local dayOfWeek = tonumber(os.date("%w")) -- get weekday as an int. Starts on sunday as it is cringe
