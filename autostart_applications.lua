4b74ad04ede4320aae91811a53d88e9d04117fd8
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-07-10 14:16:27 +0200

    added comment for misconceptions

--- autostart_applications.lua
+++ autostart_applications.lua
@@ -1,4 +1,6 @@
 
+-- to speify what commands should be run during startup, look in config_settings.lua and not here!
+
 -- do not remove these as they are a key part of the config!
 awful.spawn.with_shell("picom") -- compositor
 awful.spawn.with_shell("killall redshift ; sleep 2 ; redshift") -- has a delay at the start to make sure that it works correctly. If you don't want to use redshift then note that there already exists a button in the control panel for redshift, so you might have to tweak that one too to make the default state be off
