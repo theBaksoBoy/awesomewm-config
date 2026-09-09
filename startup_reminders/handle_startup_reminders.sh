189d188d31fb94d1ee76c173470b435580255a54
baksoBoy <128901211+theBaksoBoy@users.noreply.github.com>
2026-09-09 10:56:41 +0200

    improved startup reminder display

--- startup_reminders/handle_startup_reminders.sh
+++ startup_reminders/handle_startup_reminders.sh
@@ -4,10 +4,10 @@ FILE="/home/bakso/.config/awesome/startup_reminders/startup_reminders.txt"
 
 # check if file exists and has data
 if [ -f "$FILE" ] && [ -s "$FILE" ]; then
-    # open the file in vim in a kitty terminal
-    kitty -- vim "$FILE" &
+    # prints out the file in a kitty terminal
+    kitty --hold cat "$FILE" &
 
-    sleep 5
+    sleep 1
 
     # clear the file
     > "$FILE"
